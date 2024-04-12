9fd3795a545cd9efb8bb8476e3dcdd933a1e72ec
gparant <gregoire@workadventu.re>
2024-04-12 14:40:29 +0000

    Deploying to gh-pages from master @ 05b7d8e31299342568014dcf682f93cc29b9f0a9 🚀

---
+++ assets/starter-a8424299.js
@@ -0,0 +1,1 @@
+import{b as r}from"./init-e2054226.js";import"./translate-df78904b.js";import"./sounds-16fb2661.js";import"./config-3db041a0.js";r();WA.onInit().then(()=>{const o=WA.room.id;WA.nav.goToRoom(o.replace("starter","choice"))});
