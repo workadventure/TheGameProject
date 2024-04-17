ceea405c2ccdf23af0232628122e30861bfbc28a
gparant <gregoire@workadventu.re>
2024-04-17 12:33:27 +0000

    Deploying to gh-pages from master @ 6ebedde7c8883e009dc3155d3882671607dd8689 🚀

---
+++ assets/starter-6ac052c3.js
@@ -0,0 +1,1 @@
+import{b as r}from"./init-e2054226.js";import"./translate-df78904b.js";import"./sounds-79026e5d.js";import"./config-adf52b6e.js";r();WA.onInit().then(()=>{const o=WA.room.id;WA.nav.goToRoom(o.replace("starter","choice"))});
