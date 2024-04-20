378f437bf413d27472a9ee33dfc74d89cabe0914
Gregoire Parant <gregoire@workadventu.re>
2024-04-20 19:24:31 +0200

    Push config for production

--- src/config.ts
+++ src/config.ts
@@ -1,5 +1,5 @@
-//const rootLink = 'https://workadventure.github.io/TheGameProject'
-const rootLink = 'http://localhost:5173'
+const rootLink = 'https://workadventure.github.io/TheGameProject'
+//const rootLink = 'http://localhost:5173'
 const env: 'dev' | 'prod' =  "prod"
 
 export {
